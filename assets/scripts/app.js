019d111013b8134eaf8945d316ce904300f32963
Ali Asad <aliasad.3128@gmail.com>
2022-02-16 19:51:48 +0330

    Add overlay for sideMenu

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,21 @@
 let $ = document;
 
 const menuBtn = $.querySelector('.menu-btn');
-const sideMenu = $.querySelector('.side-menu')
+const sideMenu = $.querySelector('.side-menu');
+const sideMenuOverlay = $.querySelector('.side-menu-overlay');
 
 function sideMenuOpening(){
-    sideMenu.classList.toggle('active')
+    sideMenu.classList.add('active');
+    sideMenuOverlay.classList.add('active');
+
+}
+
+function sideMenuClosing (){
+    sideMenu.classList.remove('active');
+    sideMenuOverlay.classList.remove('active');
 }
 
 
 
-
-menuBtn.addEventListener('click', sideMenuOpening)
+menuBtn.addEventListener('click', sideMenuOpening);
+sideMenuOverlay.addEventListener('click', sideMenuClosing);
