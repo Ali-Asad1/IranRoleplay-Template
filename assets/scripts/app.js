d35a8b811cae9cc0dae432e48cb2d30464075cb8
Ali Asad <aliasad.3128@gmail.com>
2022-02-20 17:15:12 +0330

    Add DarkMode

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,20 +4,25 @@ const menuBtn = $.querySelector('.menu-btn');
 const sideMenu = $.querySelector('.side-menu');
 const sideMenuOverlay = $.querySelector('.side-menu-overlay');
 const closeBtn = $.querySelector('.close-button');
+const darkModeBtn = $.querySelector('.dark-icon');
 
-function sideMenuOpening(){
+function sideMenuOpening() {
     sideMenu.classList.add('active');
     sideMenuOverlay.classList.add('active');
 
 }
 
-function sideMenuClosing (){
+function sideMenuClosing() {
     sideMenu.classList.remove('active');
     sideMenuOverlay.classList.remove('active');
 }
 
+function darkMode() {
+    $.body.classList.toggle('dark');
+}
 
 
 menuBtn.addEventListener('click', sideMenuOpening);
 sideMenuOverlay.addEventListener('click', sideMenuClosing);
 closeBtn.addEventListener('click', sideMenuClosing);
+darkModeBtn.addEventListener('click', darkMode);
